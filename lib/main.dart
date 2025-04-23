b66a0a921343df6e9ac1a803ecb3f6ef7549f697
Enzo <enzo.baez566@gmail.com>
2025-04-23 20:02:27 -0300

    Tercer Comitt

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,18 @@
-void main() { 
-  print("Mensaje en la rama experimento");
+class Persona {
+  int id;
+  String nombre;
+  int edad;
+
+Persona(this.nombre, this.id , this.edad);
+
+
+
+
+void saludar() { 
+  print("Mi nombre es $nombre y mi edad es $edad" );
+}
+void main() {
+  var persona1 = Persona("Juan", 2 , 20);
+  persona1.saludar();
+}
 }

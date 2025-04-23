d6c5e0a48c890366f03d1bbb66b290addeec322b
Enzo <enzo.baez566@gmail.com>
2025-04-23 20:09:56 -0300

    Cuarto Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,12 @@ class Persona {
   int id;
   String nombre;
   int edad;
-
 Persona(this.nombre, this.id , this.edad);
-
-
-
-
 void saludar() { 
-  print("Mi nombre es $nombre y mi edad es $edad" );
+  print("Mi nombre es $nombre y mi edad es $edad");
+}
 }
 void main() {
   var persona1 = Persona("Juan", 2 , 20);
   persona1.saludar();
-}
 }
